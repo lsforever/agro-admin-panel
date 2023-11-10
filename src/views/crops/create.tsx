f8908a011643d9dd8ea21d124dc480401f8fe735
lsforever <lsforeverinc@gmail.com>
2023-11-11 02:10:23 +0530

    mm

--- src/views/crops/create.tsx
+++ src/views/crops/create.tsx
@@ -3,7 +3,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useFieldArray, useForm } from 'react-hook-form'
 import * as z from 'zod'
 
-import { CropIcon, Check } from 'lucide-react'
+import { CropIcon } from 'lucide-react'
 
 import {
   Dialog,
